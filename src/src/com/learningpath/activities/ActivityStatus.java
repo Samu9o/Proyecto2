ed944f851fe3d6532809a6cd5aa1570c0aaddd8c
UsuarioGithub <oscivanov@gmail.com>
2024-11-23 11:57:44 -0500

    Primer Commit Proyecto 2

---
+++ src/src/com/learningpath/activities/ActivityStatus.java
@@ -0,0 +1,9 @@
+package src.com.learningpath.activities;
+
+public enum ActivityStatus {
+    PENDING,
+    IN_PROGRESS,
+    SUBMITTED,
+    COMPLETED,
+    FAILED
+}
---
+++ src/src/com/learningpath/main/Main.java
@@ -0,0 +1,10 @@
+package src.com.learningpath.main;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleInterface ci = new ConsoleInterface();
+        ci.start();
+    }
+}
+
+
