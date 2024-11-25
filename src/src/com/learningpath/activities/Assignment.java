ee0a6a842b14ddfeb24ca20fca25d11ef3bd5cdb
UsuarioGithub <oscivanov@gmail.com>
2024-11-24 22:28:32 -0500

    Diseño

--- src/src/com/learningpath/activities/Assignment.java
+++ src/src/com/learningpath/activities/Assignment.java
@@ -16,5 +16,6 @@ public class Assignment extends Activity {
     public String getType() {
         return "Assignment";
     }
+    
 }
 	
--- src/src/com/learningpath/activities/Quiz.java
+++ src/src/com/learningpath/activities/Quiz.java
@@ -24,5 +24,6 @@ public class Quiz extends Activity {
     public String getType() {
         return "Quiz";
     }
+    
 }
 	
